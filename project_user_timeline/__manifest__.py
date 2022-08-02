7bf13c4687138dbd1f7bdbbe68c558fb94e420a9
Manuel Calero <manuelcalerosolis@gmail.com>
2022-08-02 13:40:01 +0200

    initial commit

---
+++ project_user_timeline/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project User Timeline - Timesheet",
+    "summary": "Shows the progress of tasks on the timeline view.",
+    "author": "Xtendoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/project",
+    "category": "Project Management",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "project_timeline",
+        "hr_timesheet"
+    ],
+    "data": [ "views/project_task_view.xml"],
+    "installable": True,
+    "auto_install": True,
+}
