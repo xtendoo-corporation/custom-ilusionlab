fc0e0ed21c74a21c54b4f7905f849ac81fbe70c0
Manuel Calero <manuelcalerosolis@gmail.com>
2022-08-04 12:19:23 +0200

    project name

--- project_user_timeline/__init__.py
+++ project_user_timeline/__init__.py
@@ -1,2 +1,1 @@
-# Copyright 2022 Manuel Calero (<http://xtendoo.es>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import models
---
+++ project_user_timeline/models/project.py
@@ -0,0 +1,11 @@
+from odoo import models, fields, api, _
+
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    project_name = fields.Char(
+        string="Project name",
+        related='project_id.name',
+        readonly=False,
+    )
